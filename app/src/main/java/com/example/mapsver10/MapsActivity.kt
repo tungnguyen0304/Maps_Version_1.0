0fa0672d6241a95ace8b87c22b0a783762df4f2f
LAM TUNG <daothang200@gmail.com>
2022-05-15 15:09:01 +0700

    Initial Commit

--- app/src/main/java/com/example/mapsver10/MapsActivity.kt
+++ app/src/main/java/com/example/mapsver10/MapsActivity.kt
@@ -1,30 +1,46 @@
 package com.example.mapsver10
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.Resources
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
+import com.example.mapsver10.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.example.mapsver10.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.model.*
 import java.util.*
 
+
+@Suppress("PrivatePropertyName")
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
-    private val TAG = MapsActivity::class.java.simpleName
+    private val tag = MapsActivity::class.java.simpleName
+
+    // creating variables for
+    // edit texts and button.
+    private lateinit var sourceEdt: EditText
+    private lateinit var destinationEdt: EditText
+    private lateinit var trackBtn: Button
+
     private fun setMapStyle(map: GoogleMap) {
         try {
             // Customize the styling of the base map using a JSON object defined
@@ -37,20 +53,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             )
 
             if (!success) {
-                Log.e(TAG, "Style parsing failed.")
+                Log.e(tag, "Style parsing failed.")
             }
         } catch (e: Resources.NotFoundException) {
-            Log.e(TAG, "Can't find style. Error: ", e)
+            Log.e(tag, "Can't find style. Error: ", e)
         }
     }
-    /*  private fun setMapLongClick(map: GoogleMap) {
-        map.setOnMapLongClickListener { latLng ->
-            map.addMarker(
-                MarkerOptions()
-                    .position(latLng)
-            )
-        }
-    } */
+
     private fun setMapLongClick(map: GoogleMap) {
         map.setOnMapLongClickListener { latLng ->
             // A snippet is additional text that's displayed after the title.
@@ -68,24 +77,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             )
         }
     }
+
     private fun setPoiClick(map: GoogleMap) {
         map.setOnPoiClickListener { poi ->
-            val poiMarker = map.addMarker(
+            map.addMarker(
                 MarkerOptions()
                     .position(poi.latLng)
                     .title(poi.name)
-            )
-            if (poiMarker != null) {
-                poiMarker.showInfoWindow()
-            }
+            )?.showInfoWindow()
         }
     }
-    private val REQUEST_LOCATION_PERMISSION = 1
+
+    private val RequestLocationPermission = 1
     private fun isPermissionGranted() : Boolean {
         return ContextCompat.checkSelfPermission(
             this,
             Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
+    @SuppressLint("MissingPermission")
     private fun enableMyLocation() {
         if (isPermissionGranted()) {
             map.isMyLocationEnabled = true
@@ -93,8 +102,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         else {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_LOCATION_PERMISSION
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                RequestLocationPermission
             )
         }
     }
@@ -105,40 +114,86 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // initializing our edit text and buttons
+        sourceEdt = findViewById(R.id.idEdtSource)
+        destinationEdt = findViewById(R.id.idEdtDestination)
+        trackBtn = findViewById(R.id.idBtnTrack)
+
+        // adding on click listener to our button.
+        // adding on click listener to our button.
+        trackBtn.setOnClickListener { // calling a method to draw a track on google maps.
+            drawTrack(sourceEdt.text.toString(), destinationEdt.text.toString())
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun drawTrack(source: String, destination: String) {
+        try {
+            // create a uri
+            val uri = Uri.parse("https://www.google.co.in/maps/dir/$source/$destination")
+
+            // initializing a intent with action view.
+            val i = Intent(Intent.ACTION_VIEW, uri)
+
+            // below line is to set maps package name
+            i.setPackage("com.google.android.apps.maps")
+
+            // below line is to set flags
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            // start activity
+            startActivity(i)
+        } catch (e: ActivityNotFoundException) {
+            // when the google maps is not installed on users device
+            // we will redirect our user to google play to download google maps.
+            val uri =
+                Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps")
+
+            // initializing intent with action view.
+            val i = Intent(Intent.ACTION_VIEW, uri)
+
+            // set flags
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            // to start activity
+            startActivity(i)
+        }
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
+        Toast.makeText(this, "Map is Ready",  Toast.LENGTH_SHORT).show()
+        Log.d(tag, "onMapReady: map is ready")
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
         val home = LatLng(10.868173, 106.637558)
         val zoomLevel = 15f
         map.addMarker(MarkerOptions().position(home).title("Home"))
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(home,zoomLevel))
 
-        // Add a ground overlay
-        val overlaySize = 100f
-        val androidOverlay = GroundOverlayOptions()
-            .image(BitmapDescriptorFactory.fromResource(R.drawable.android))
-            .position(home, overlaySize)
+        val cameraPosition = CameraPosition.Builder()
+            .target(home)
+            .zoom(15f).bearing(67f).tilt(45f).build()
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+        map.addCircle(
+            CircleOptions()
+                .center(home)
+                .radius(500.0)
+                .strokeWidth(3f)
+                .strokeColor(Color.BLACK)
+                .fillColor(Color.argb(70,150,50,50))
+        )
 
         setMapLongClick(map)
         setPoiClick(map)
         setMapStyle(map)
-        map.addGroundOverlay((androidOverlay))
         enableMyLocation()
     }
 
@@ -174,12 +229,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         permissions: Array<String>,
         grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+        if (requestCode == RequestLocationPermission) {
             if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
                 enableMyLocation()
             }
         }
     }
-
 
 }
